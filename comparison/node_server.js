b06f9964bfad1820061f145d31bc43acaf43e1bb
Rocco Bowling <rocco@chimerasw.com>
2020-01-29 09:54:16 -0500

    adding crystal and node "hello world" http servers for comparison testing

---
+++ comparison/node_server.js
@@ -0,0 +1,14 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello World');
+  }).listen(8080);
+}
